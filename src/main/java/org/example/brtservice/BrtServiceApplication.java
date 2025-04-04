9b49a52c19a5c59c9a7a2291231987f42bce20de
is6769 <igor.shvets2007@mail.ru>
2025-04-04 13:33:01 +0300

    first commit

---
+++ src/main/java/org/example/brtservice/BrtServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.brtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrtServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/brtservice/BrtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.brtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
