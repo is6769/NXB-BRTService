47309490c48db759c78027d09f90a0e826723830
is6769 <igor.shvets2007@mail.ru>
2025-05-09 20:34:12 +0300

    solved all todos

--- src/main/resources/db/changelog/changeset/0002-create-table-cdrs.sql
+++ src/main/resources/db/changelog/changeset/0002-create-table-cdrs.sql
@@ -3,7 +3,7 @@
 create table if not exists cdrs(
     id                  BIGSERIAL       PRIMARY KEY,
     call_type           VARCHAR(3)      NOT NULL,
-    serviced_msisdn     VARCHAR(50)     NOT NULL, --TODO remake fields
+    serviced_msisdn     VARCHAR(50)     NOT NULL,
     other_msisdn        VARCHAR(50)     NOT NULL,
     start_date_time     TIMESTAMP       NOT NULL,
     finish_date_time    TIMESTAMP       NOT NULL
